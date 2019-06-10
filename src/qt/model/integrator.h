58dd2332d58d2201581fb66e355104bc6810a4fa
Artem Volkov <artvolkov96@yandex.ru>
2019-06-10 22:28:13 +0300

    Divide integrator class into two files

--- src/qt/model/integrator.h
+++ src/qt/model/integrator.h
@@ -1,24 +1,13 @@
 class Integrator {
 
 public:
-	Integrator(double init_state, double init_in): 
-        state(init_state),
-        prev_in(init_in)
-    {}
+	Integrator(double init_state, double init_in);
 
-	void update(double input, double dt) {
-        this->state_prev = this->state;
-        this->state = state_prev + input*dt;
-        this->prev_in = input;
-	}
+	void update(double input, double dt);
 
-    double getState_prev() {
-		return this->state_prev;
-	}
+    double getState_prev();
 
-	double getState() {
-		return this->state;
-	}
+	double getState();
 
 private:
 	double state;
